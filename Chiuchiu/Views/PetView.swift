bcb5c9d26043fd8b04aa8e53bd621f1228a80639
李铭宸 <3072126900@qq>
2026-07-26 14:00:16 +0800

    Add Chiuchiu blinking behavior

--- Chiuchiu/Views/PetView.swift
+++ Chiuchiu/Views/PetView.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 struct PetView: View {
     @State private var isBreathing = false
+    @State private var isBlinking = false
+    @State private var blinkTimer: Timer?
+    @State private var blinkEndTimer: Timer?
 
     var body: some View {
-        Image("ChiuChiuIdleFront")
+        Image(isBlinking ? "ChiuChiuBlink" : "ChiuChiuIdleFront")
             .resizable()
             .scaledToFit()
             .frame(width: 220, height: 220)
@@ -16,8 +19,50 @@ struct PetView: View {
             )
             .onAppear {
                 isBreathing = true
+                scheduleNextBlink()
+            }
+            .onDisappear {
+                stopBlinkTimers()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color.clear)
+    }
+
+    private func scheduleNextBlink() {
+        blinkTimer?.invalidate()
+        let timer = Timer(
+            timeInterval: Double.random(in: 5...10),
+            repeats: false
+        ) { _ in
+            MainActor.assumeIsolated {
+                isBlinking = true
+                scheduleBlinkEnd()
+            }
+        }
+        blinkTimer = timer
+        RunLoop.main.add(timer, forMode: .common)
+    }
+
+    private func scheduleBlinkEnd() {
+        blinkEndTimer?.invalidate()
+        let timer = Timer(
+            timeInterval: Double.random(in: 0.15...0.25),
+            repeats: false
+        ) { _ in
+            MainActor.assumeIsolated {
+                isBlinking = false
+                scheduleNextBlink()
+            }
+        }
+        blinkEndTimer = timer
+        RunLoop.main.add(timer, forMode: .common)
+    }
+
+    private func stopBlinkTimers() {
+        blinkTimer?.invalidate()
+        blinkEndTimer?.invalidate()
+        blinkTimer = nil
+        blinkEndTimer = nil
+        isBlinking = false
     }
 }
